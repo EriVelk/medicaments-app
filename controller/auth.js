7df3c859cae924b4f269f7df1db55f6fcc8ad687
EriVelk <erve@live.com.mx>
2022-03-26 19:54:07 -0600

    Validation with Express-Validator

--- controller/auth.js
+++ controller/auth.js
@@ -1,41 +1,55 @@
 const Doctor = require('../models/Doctor');
 const cryptoJS = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const { body, validationResult, check } = require('express-validator')
 
 const authController = {
-    authControllerRegister: async (req, res) => {
-        const email = req.body.email;
-        const exist = await Doctor.findOne({email:email});
+    authControllerRegister: [
+        body('name').trim().isLength({ min: 3 }).escape(),
+        body('surname').trim().isLength({ min: 3 }).escape(),
+        body('secondsurname').trim().isLength({ min: 3 }).escape(),
+        body('password').trim().not().isEmpty().escape(),
+        check('email').trim().isEmail().normalizeEmail().escape().withMessage('Invalid Email').custom(async(email) => {
+            const emailDoctor = await Doctor.findOne({ email: email });
+            if (emailDoctor) {
+                throw new Error('Email al ready in use.');
+            }
+        }),
 
-        console.log(exist)
+        async (req, res) => {
 
-        if(exist){
-            res.status(403).json({"message":"El correo ya esta registrado."})
-        }else{
-            const newDoctor = new Doctor({
-                name: req.body.name,
-                surname: req.body.surname,
-                secondsurname: req.body.secondsurname,
-                email: email,
-                password: cryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
-            })
-    
-            try {
-                const savedDoctor = await newDoctor.save();
-                res.status(201).json(savedDoctor);
-            } catch (error) {
-                res.status(500).json("Error:::: ", error);
+            const errors = validationResult(req);
+
+            if (errors.isEmpty()) {
+                const email = req.body.email;
+                const newDoctor = new Doctor({
+                    name: req.body.name,
+                    surname: req.body.surname,
+                    secondsurname: req.body.secondsurname,
+                    email: email,
+                    password: cryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+                })
+
+                try {
+                    const savedDoctor = await newDoctor.save();
+                    res.status(201).json(savedDoctor);
+                } catch (error) {
+                    res.status(500).json("Error:::: ", error);
+                }
+                
+            }else{
+                res.status(403).json({errors: errors.array()});
             }
         }
-    },
-    
+    ],
+
     authControllerLogin: async (req, res) => {
         try {
             const doctor = await Doctor.findOne({
                 email: req.body.email
             })
-            
-            !doctor && res.status(401).json({"message":"Wrong credentials"});
+
+            !doctor && res.status(401).json({ "message": "Wrong credentials" });
 
             const hashedPassword = cryptoJS.AES.decrypt(
                 doctor.password,
@@ -44,36 +58,36 @@ const authController = {
 
             const originalPassword = hashedPassword.toString(cryptoJS.enc.Utf8);
 
-            originalPassword !== req.body.password && res.status(401).json({"message":"Wrong credentials"});
+            originalPassword !== req.body.password && res.status(401).json({ "message": "Wrong credentials" });
 
             const accessToken = jwt.sign(
                 {
-                    id:doctor._id,
+                    id: doctor._id,
                     isadmin: doctor.isadmin
                 },
                 process.env.JWT_SEC,
                 {
-                    expiresIn:"3d"
+                    expiresIn: "3d"
                 }
             )
 
-            const{password, ...others} = doctor._doc;
-            
-            res.status(200).cookie('access_token','Bearer ' + accessToken, {
+            const { password, ...others } = doctor._doc;
+
+            res.status(200).cookie('access_token', 'Bearer ' + accessToken, {
                 expires: new Date(Date.now() + 8 * 3600000) // cookie will be removed after 8 hours
-              }).json({...others, accessToken})
+            }).json({ ...others, accessToken })
 
         } catch (error) {
-            
+
             res.status(500).json(error);
             console.log(error);
-        
+
         }
     },
 
-    getAllUsers: async(req, res) =>{
+    getAllUsers: async (req, res) => {
         try {
-            const doctors = await Doctor.find().populate({path:"listPacient", model:"Pacient"});
+            const doctors = await Doctor.find().populate({ path: "listPacient", model: "Pacient" });
             res.status(200).json(doctors);
         } catch (error) {
             res.status(500).json(error);
